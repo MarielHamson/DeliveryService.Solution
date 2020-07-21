f6f77d0d1b2850dd2f84c539441256fad00c29ec
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-21 14:23:47 -0700

    fail test for weight dimension
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: Zentraxius <clanalia55@gmail.com>

--- PackageDelivery.Tests/ModelTests/ParcelTests.cs
+++ PackageDelivery.Tests/ModelTests/ParcelTests.cs
@@ -9,7 +9,7 @@ namespace PackageDelivery.Tests
     [TestMethod]
     public void ParcelConstructor_DoesParcelExist_Parcel()
     {
-      Parcel parcelTest = new Parcel(9, 10, 11);
+      Parcel parcelTest = new Parcel(9, 10, 11, 10);
       Assert.AreEqual(typeof(Parcel), parcelTest.GetType());
     }
 
@@ -19,13 +19,19 @@ namespace PackageDelivery.Tests
       int height = 10;
       int width = 15;
       int depth = 10;
-      Parcel parcelTest = new Parcel(10, 15, 10);
+      Parcel parcelTest = new Parcel(10, 15, 10, 10);
       int testHeight = parcelTest.Height;
       int testWidth = parcelTest.Width;
       int testDepth = parcelTest.Depth;
       Assert.AreEqual(height, testHeight);
       Assert.AreEqual(width, testWidth);
       Assert.AreEqual(depth, testDepth);
+    }
+    [TestMethod]
+    public void GetWeight_ShouldHaveWeight_BeWeighty()
+    {
+      Parcel parcelTest = new Parcel(10, 15, 10, 20);
+      Assert.AreEqual(parcelTest.Weight, 10);
     }
   }
 }
--- PackageDelivery/Models/Parcel.cs
+++ PackageDelivery/Models/Parcel.cs
@@ -5,12 +5,14 @@ namespace PackageDelivery.Models
     public int Height { get; set; }
     public int Width { get; set; }
     public int Depth { get; set; }
+    public int Weight { get; set; }
 
-    public Parcel(int height, int width, int depth)
+    public Parcel(int height, int width, int depth, int weight)
     {
       Height = height;
       Width = width;
       Depth = depth;
+      Weight = weight;
     }
     // properties, methods, etc. will go here.
 
