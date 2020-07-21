c6375658f73bb14ce65805afef8f49e4e056eacc
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-21 13:58:51 -0700

    add failing test for dimension properties
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: Zentraxius <clanalia55@gmail.com>

--- PackageDelivery.Tests/ModelTests/ParcelTests.cs
+++ PackageDelivery.Tests/ModelTests/ParcelTests.cs
@@ -7,10 +7,25 @@ namespace PackageDelivery.Tests
   public class ParcelTests
   {
     [TestMethod]
-    public void ParcelMeasure_DoesParcelHaveDimensions_ItShould()
+    public void ParcelConstructor_DoesParcelExist_Parcel()
     {
-    Parcel parcelTest = new Parcel();
-    Assert.AreEqual(typeof(Parcel), parcelTest.GetType());
+      Parcel parcelTest = new Parcel(9, 10, 11);
+      Assert.AreEqual(typeof(Parcel), parcelTest.GetType());
+    }
+
+    [TestMethod]
+    public void GetDimensions_ReturnDimensions_Int()
+    {
+      int height = 10;
+      int width = 15;
+      int depth = 10;
+      Parcel parcelTest = new Parcel(9, 10, 11);
+      int testHeight = parcelTest.Height;
+      int testWidth = parcelTest.Width;
+      int testDepth = parcelTest.Depth;
+      Assert.AreEqual(height, testHeight);
+      Assert.AreEqual(width, testWidth);
+      Assert.AreEqual(depth, testDepth);
     }
   }
 }
--- PackageDelivery/Models/Parcel.cs
+++ PackageDelivery/Models/Parcel.cs
@@ -2,7 +2,16 @@ namespace PackageDelivery.Models
 {
   public class Parcel
   {
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public int Depth { get; set; }
 
+    public Parcel(int height, int width, int depth)
+    {
+      Height = height;
+      Width = width;
+      Depth = depth;
+    }
     // properties, methods, etc. will go here.
 
   }
