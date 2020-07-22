41bc39148a9e0fe6dc1b2a83283edfaf07f374f9
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-22 10:19:48 -0700

    revise checkout to display findPrice
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>

--- PackageDelivery/Controllers/ParcelController.cs
+++ PackageDelivery/Controllers/ParcelController.cs
@@ -13,37 +13,13 @@ namespace PackageDelivery.Controllers
       return View();
     }
 
-    // [HttpGet("/checkout")]
-    // public ActionResult Checkout(int height, int width, int length, int weight)
-    // {
-    //   Parcel newParcel = new Parcel(height, width, length, weight);
-    //   int parcelVolume = newParcel.findVolume();
-    //   int parcelPrice = newParcel.findPrice(parcelVolume);
-    //   Console.WriteLine(parcelPrice);
-    //   // return View(parcelPrice);
-
     [HttpPost("/checkout")]
     public ActionResult Checkout(int height, int width, int length, int weight)
     {
       Parcel newParcel = new Parcel(height, width, length, weight);
       int parcelVolume = newParcel.findVolume();
-      int parcelPrice = newParcel.findPrice(parcelVolume);
-      return View(parcelPrice);
+      newParcel.findPrice(parcelVolume);
+      return View(newParcel);
     }
   }
 }
-
-
-// public class HomeController : Controller
-// {
-
-//   [Route("/")]
-//   public ActionResult Form() { return View(); }
-
-//   [Route("/result")]
-//   public ActionResult Result(string adj1, string food1, string vegetable1, string food2, string vegetable2, string verb1, string verb2, string vegetable3, string vegetable4, string verb3)
-//   {
-//     MadLibsVariable newMadLibs = new MadLibsVariable(adj1, food1, vegetable1, food2, vegetable2, verb1, verb2, vegetable3, vegetable4, verb3);
-//     return View(newMadLibs);
-//   }
-// }
--- PackageDelivery/Models/Parcel.cs
+++ PackageDelivery/Models/Parcel.cs
@@ -2,10 +2,10 @@ namespace PackageDelivery.Models
 {
   public class Parcel
   {
-    public int Height { get; set; }
-    public int Width { get; set; }
-    public int Length { get; set; }
-    public int Weight { get; set; }
+    public int Height { get; set; } = 0;
+    public int Width { get; set; } = 0;
+    public int Length { get; set; } = 0;
+    public int Weight { get; set; } = 0;
 
     public Parcel(int height, int width, int length, int weight)
     {
